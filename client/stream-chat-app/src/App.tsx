22c2359ed5f007567897c016871b036d4e0a49f5
Jaivardhan Deshwal <deshwaljaivardhan@gmail.com>
2023-01-26 23:37:16 +0530

    initial commit

---
+++ client/stream-chat-app/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-3xl font-bold underline">
+      YOLOOOO
+    </div>
+  )
+}
+
+export default App
